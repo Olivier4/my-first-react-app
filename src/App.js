6ebf7f171a2e517729f072378e4e800629bae91f
Olivier <oliviervanthof@gmail.com>
2019-04-01 15:25:36 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+
           <img src={logo} className="App-logo" alt="logo" />
           <p>
       Hello
